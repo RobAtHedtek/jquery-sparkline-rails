5d49f45f0afe56c16752e4aecfe585c45d04b98a
RobAtHedtek <rob@hedtek.com>
2012-08-07 17:00:10 +0100

    Oops - forgot Engine

--- lib/jquery/sparkline/rails/engine.rb
+++ lib/jquery/sparkline/rails/engine.rb
@@ -2,6 +2,8 @@ module Jquery
   module Sparkline
     if defined?(::Rails) and ::Rails.version >= "3.1"
       module Rails
+        class Engine < ::Rails::Engine
+        end
       end
     end
   end
