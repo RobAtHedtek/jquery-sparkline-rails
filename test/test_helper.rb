7786a1200564d6a56257472de416d425dbd791cc
Robert Burrell Donkin <rob@hedtek>
2012-08-08 17:30:18 +0100

    Switch to mini test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,13 @@
 # Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
-
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+require 'rails/test_help'
+require 'minitest/autorun'
+require "capybara/rails"
 
 Rails.backtrace_cleaner.remove_silencers!
 
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+class IntegrationTest < MiniTest::Spec
+  include Capybara::DSL
+  register_spec_type(/integration$/, self)
 end
