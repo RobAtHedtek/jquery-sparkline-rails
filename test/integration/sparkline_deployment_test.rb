799670994e2fe01db591384c770117fee59b819b
Robert Burrell Donkin <rob@hedtek>
2012-08-08 17:42:14 +0100

    Update regex so that it finds our test

--- test/integration/sparkline_deployment_test.rb
+++ test/integration/sparkline_deployment_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 describe "sparkline javascript deployment" do
   it "should include jquery.sparkline.js on the asset pipeline" do
-    #visit '/assets/sparkline/jquery.sparkline.js'
-    #page.text.must_include 'http://omnipotent.net/jquery.sparkline/'
+    visit '/assets/sparkline/jquery.sparkline.js'
+    page.text.must_include 'http://omnipotent.net/jquery.sparkline/'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,12 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 require 'rails/test_help'
 require 'minitest/autorun'
-require "capybara/rails"
+require 'capybara/rails'
 
 Rails.backtrace_cleaner.remove_silencers!
 
 class IntegrationTest < MiniTest::Spec
   include Capybara::DSL
   register_spec_type(/integration$/, self)
+  register_spec_type(/deployment$/, self)
 end
