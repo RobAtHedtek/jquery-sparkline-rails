03cf84e852c16d0637a2a313eb5af696a3377ebd
RobAtHedtek <rob@hedtek.com>
2012-08-09 16:45:54 +0100

    Improve test coverage

--- test/integration/sparkline_deployment_test.rb
+++ test/integration/sparkline_deployment_test.rb
@@ -5,4 +5,15 @@ describe "sparkline javascript deployment" do
     visit '/assets/sparkline/jquery.sparkline.js'
     page.text.must_include 'http://omnipotent.net/jquery.sparkline/'
   end
+
+  it "should include sparkline on the asset pipeline" do
+    visit '/assets/sparkline/'
+    page.text.must_include 'http://omnipotent.net/jquery.sparkline/'
+  end
+
+  it "should include sparkline on the asset pipeline" do
+    visit '/assets/sparkline.js'
+    page.text.must_include 'http://omnipotent.net/jquery.sparkline/'
+  end
+
 end
